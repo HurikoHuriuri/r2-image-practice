a2d957e04577b3282b0456fac0a02c7e4601874b
huriko <huriko300@outlook.jp>
2024-02-12 00:27:35 +0900

    create page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,7 @@ import Image from "next/image"
 import { useState } from "react";
 export default function Home() {
   const [uploadImage, setImage] = useState("/remove.svg");
-  const onFileInputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+  const onInputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
     if (!e.target.files) return;
     const file = e.target.files[0];
     setImage(window.URL.createObjectURL(file));
@@ -17,14 +17,11 @@ export default function Home() {
           id="r2" name="r2"
           accept="image/png, image/jpeg"
           className="file-input w-full max-w-xs"
-          onChange={onFileInputChange} />
+          onChange={onInputChange} />
         <button className="btn btn-primary flex-0">アップロード</button>
       </div>
       <div className="divider lg:divider-horizontal h-full"></div>
-      <div className="flex flex-1">
-        <Image src={uploadImage} width={500} height={500} alt="upload image"></Image>
-        <div className="flex-1"></div>
-      </div>
+      <Image src={uploadImage} width={500} height={500} alt="upload image" className="flex-1"></Image>
     </div>
   )
 }
